ee94ecb3970272b012e7f1089fb5ba13b9ccd0f9
Panuwat Fhamongkolchai <panuwat.fmkc@mail.kmutt.ac.th>
2020-05-05 07:31:38 -0700

    first commit

---
+++ KFC/Services/ForgotpasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgotpasswordViewController.swift
+//  KFC
+//
+//  Created by ภาณุวัฒน์ ฟ้ามงคลชัย on 5/5/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import UIKit
+
+class ForgotpasswordViewController: UIViewController {
+
+    @IBAction func userEmailTextField(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recoverButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
